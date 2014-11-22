efefed13e5051079e6b7624bd8668ec98c260417
dpolyakov <dima@my-html.ru>
2014-11-23 01:10:19 +0300

    Редиректы для категорий

--- ucoz-permalink-migration.php
+++ ucoz-permalink-migration.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: uCoz permalink migration
 Description: Правильные редиректы с юкозовских урлов.
-Version: 1.0
+Version: 1.1
 Author: Dmitry Polyakov
 Author URI: http://my-html.ru
 */
@@ -18,28 +18,39 @@ function external_redirect($query)
 
 function internal_redirect($query)
 {
-    $post_id = 0;
     $patterns = array("/index/", "/blog/", "'\/'");
     $query = preg_replace($patterns, '', $query);
 
     $params = explode("-", $query);
     $params_count = count($params);
 
+    // "Странный урл 86-" или страница
     if ($params_count == 4 || $params_count == 2) {
-        $post_id = $params[1];
+        $permalink = get_permalink($params[1]);
     }
 
+    // Пост с множеством комментов
     if ($params_count == 6) {
-        $post_id = $params[3];
+        $permalink = get_permalink($params[3]);
     }
 
-    if ($post_id) {
-        $permalink = get_permalink($post_id);
+    // Скорее всего это категория
+    if ($params_count == 3) {
+        $category = get_category_by_slug($query);
+        $category_id = $category->term_id;
+        $permalink = get_category_link($category_id);
+    }
 
-        if (strcasecmp($permalink, '') != 0) {
-            wp_redirect($permalink, 301);
-            exit;
-        }
+    if ($permalink) {
+        do_internal_redirect($permalink);
+    }
+}
+
+function do_internal_redirect($permalink)
+{
+    if (strcasecmp($permalink, '') != 0) {
+        wp_redirect($permalink, 301);
+        exit;
     }
 }
 
